c6b7b46751bf833567b21bbae2d4c542e2ef9092
PoeHut <poeeiphyu77@gmail.com>
2021-01-09 06:11:47 +0630

    adding middleware in redux

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { Provider } from 'react-redux';
-import {createStore}  from 'redux'
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware}  from 'redux'
+import thunk from 'redux-thunk'
 import reducers  from './src/reducers'
-import Routes from './src/Routes';
+import Routes  from './src/Routes'
 
 const App = () => {
    return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
         <Routes />
     </Provider>
    )
